43fe254b64590b0b2fd79d569dbe17ac96586b60
chylex <contact@chylex.com>
2022-06-10 09:51:56 +0200

    Add build and plugin configuration

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	kotlin("jvm") version "1.6.10"
+	id("org.jetbrains.intellij") version "1.6.0"
+}
+
+group = "com.chylex.intellij.rider.vcsgroupbyproject"
+version = "0.0.1"
+
+repositories {
+	mavenCentral()
+}
+
+intellij {
+	type.set("RD")
+	version.set("2022.1")
+	updateSinceUntilBuild.set(false)
+}
+
+tasks.buildSearchableOptions {
+	enabled = false
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions.jvmTarget = "11"
+	kotlinOptions.freeCompilerArgs = listOf(
+		"-Xjvm-default=enable"
+	)
+}
