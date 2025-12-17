39e5e804080b1b0eb24d274af0ff5c3af1806930
chylex <contact@chylex.com>
2025-12-17 23:45:00 +0100

    Update for Rider 2025.3

--- src/main/kotlin/com/chylex/intellij/rider/vcsgroupbyproject/ProjectChangesGroupingPolicy.kt
+++ src/main/kotlin/com/chylex/intellij/rider/vcsgroupbyproject/ProjectChangesGroupingPolicy.kt
@@ -1,17 +1,13 @@
 package com.chylex.intellij.rider.vcsgroupbyproject
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.NotNullLazyKey
-import com.intellij.openapi.vcs.FilePath
-import com.intellij.openapi.vcs.changes.ui.BaseChangesGroupingPolicy
 import com.intellij.openapi.vcs.changes.ui.ChangesBrowserNode
 import com.intellij.openapi.vcs.changes.ui.ChangesGroupingPolicyFactory
+import com.intellij.openapi.vcs.changes.ui.SimpleChangesGroupingPolicy
 import com.intellij.openapi.vcs.changes.ui.StaticFilePath
-import com.intellij.openapi.vcs.changes.ui.TreeModelBuilder
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.backend.workspace.WorkspaceModel
-import com.intellij.vcsUtil.VcsUtil
-import com.jetbrains.rider.model.RdCustomLocation
+import com.intellij.vcsUtil.VcsImplUtil
 import com.jetbrains.rider.model.RdProjectDescriptor
 import com.jetbrains.rider.model.RdProjectModelItemDescriptor
 import com.jetbrains.rider.model.RdUnloadProjectDescriptor
@@ -22,41 +18,25 @@ import com.jetbrains.rider.projectView.workspace.isProject
 import com.jetbrains.rider.projectView.workspace.isProjectFile
 import com.jetbrains.rider.projectView.workspace.isProjectFolder
 import com.jetbrains.rider.projectView.workspace.isUnloadedProject
-import java.io.File
 import javax.swing.tree.DefaultTreeModel
 
-class ProjectChangesGroupingPolicy(private val project: Project, private val model: DefaultTreeModel) : BaseChangesGroupingPolicy() {
-	override fun getParentNodeFor(nodePath: StaticFilePath, subtreeRoot: ChangesBrowserNode<*>): ChangesBrowserNode<*>? {
-		val nextPolicyParent = nextPolicy?.getParentNodeFor(nodePath, subtreeRoot)
-		
-		val file = resolveVirtualFile(nodePath)
+class ProjectChangesGroupingPolicy(private val project: Project, model: DefaultTreeModel) : SimpleChangesGroupingPolicy<RdProjectModelItemDescriptor>(model) {
+	override fun getGroupRootValueFor(nodePath: StaticFilePath, node: ChangesBrowserNode<*>): RdProjectModelItemDescriptor? {
+		val file = VcsImplUtil.findValidParentAccurately(nodePath.filePath)
 		if (file == null) {
-			return nextPolicyParent
+			return null
 		}
 		
 		val descriptor = getSingleProjectEntity(file, project)?.descriptor
 		if (descriptor !is RdProjectDescriptor && descriptor !is RdUnloadProjectDescriptor) {
-			return nextPolicyParent
+			return null
 		}
 		
-		val grandParent = nextPolicyParent ?: subtreeRoot
-		val cachingRoot = getCachingRoot(grandParent, subtreeRoot)
-		
-		return NODE_CACHE.getValue(cachingRoot).getOrPut(descriptor) {
-			ChangesBrowserProjectNode(descriptor).also {
-				it.markAsHelperNode()
-				model.insertNodeInto(it, grandParent, grandParent.childCount)
-				NODE_CACHE.getValue(it)[descriptor] = it
-				
-				val projectFolder = getFolder(descriptor)
-				if (projectFolder != null) {
-					val folderKey = TreeModelBuilder.staticFrom(projectFolder).key
-					TreeModelBuilder.DIRECTORY_CACHE.getValue(cachingRoot)[folderKey] = it
-					TreeModelBuilder.IS_CACHING_ROOT.set(it, true)
-					TreeModelBuilder.DIRECTORY_CACHE.getValue(it)[folderKey] = it
-				}
-			}
-		}
+		return descriptor
+	}
+	
+	override fun createGroupRootNode(value: RdProjectModelItemDescriptor): ChangesBrowserNode<*> {
+		return ChangesBrowserProjectNode(value).also { it.markAsHelperNode() }
 	}
 	
 	internal class Factory : ChangesGroupingPolicyFactory() {
@@ -64,10 +44,6 @@ class ProjectChangesGroupingPolicy(private val project: Project, private val mod
 	}
 	
 	private companion object {
-		private val NODE_CACHE = NotNullLazyKey.createLazyKey<MutableMap<RdProjectModelItemDescriptor?, ChangesBrowserNode<*>>, ChangesBrowserNode<*>>("ChangesTree.ProjectCache") {
-			mutableMapOf()
-		}
-		
 		private fun getSingleProjectEntity(file: VirtualFile, project: Project): ProjectModelEntity? {
 			val workspaceModel = WorkspaceModel.getInstance(project)
 			val entities = walkFileParentsUntilResultIsNotEmpty(file) { workspaceModel.getProjectModelEntities(it, project) }
@@ -100,18 +76,6 @@ class ProjectChangesGroupingPolicy(private val project: Project, private val mod
 		
 		private fun isProjectOrProjectFile(entity: ProjectModelEntity): Boolean {
 			return entity.isProjectFile() || entity.isProjectFolder() || entity.isProject() || entity.isUnloadedProject()
-		}
-		
-		private fun getFolder(descriptor: RdProjectModelItemDescriptor): FilePath? {
-			val location = descriptor.location
-			if (location !is RdCustomLocation) {
-				return null
-			}
-			
-			val projectFile = File(location.customLocation)
-			val projectFolder = projectFile.parent
-			
-			return projectFolder?.let(VcsUtil::getFilePath)
 		}
 	}
 }
--- src/main/kotlin/com/chylex/intellij/rider/vcsgroupbyproject/SetProjectChangesGroupingAction.kt
+++ src/main/kotlin/com/chylex/intellij/rider/vcsgroupbyproject/SetProjectChangesGroupingAction.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.vcs.changes.ui.ChangesGroupingSupport
 
 class SetProjectChangesGroupingAction : SetChangesGroupingAction() {
 	/**
-	 * Uses [ChangesGroupingSupport.MODULE_GROUPING] because of [com.intellij.openapi.vcs.changes.ui.PREDEFINED_PRIORITIES].
+	 * Uses [ChangesGroupingSupport.MODULE_GROUPING] because of [com.intellij.openapi.vcs.changes.ui.ChangesTree.DEFAULT_GROUPING_KEYS].
 	 */
 	override val groupingKey
 		get() = ChangesGroupingSupport.MODULE_GROUPING
